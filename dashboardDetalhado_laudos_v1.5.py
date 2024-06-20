32d916420f88617c513f4e439233b0f5f5e2f03c
adm-so-ted-incra-ufpr <167918295+adm-so-ted-incra-ufpr@users.noreply.github.com>
2024-06-20 16:14:13 -0300

    Update dashboardDetalhado_laudos_v1.5.py

--- dashboardDetalhado_laudos_v1.5.py
+++ dashboardDetalhado_laudos_v1.5.py
@@ -13,12 +13,19 @@ st.title("(SO - TED INCRA/UFPR) - Laudos de Supervisão Ocupacional ")
 # Definir título da tabela com informações gerais sobre os laudos
 st.subheader("Relação de laudos")
 
+# Função para filtrar opções com base na seleção de um filtro específico
+def filter_options(selected_filter, column_name):
+    options = ['Todos'] + sorted(list(df[column_name].unique()))
+    if selected_filter != "Todos":
+        options = [selected_filter] + [x for x in options if x != selected_filter]
+    return options
+
 # Ordenar opções de pesquisa
-tecnicos = ['Todos'] + sorted(list(df['Técnico'].unique()))
-assentamentos = ['Todos'] + sorted(list(df['Assentamento'].unique()))
-tipos_de_laudo = ['Todos'] + sorted(list(df['Tipo de Laudo'].unique()))
-municipios = ['Todos'] + sorted(list(df['Município'].unique()))
-modalidade = ['Todos'] + sorted(list(df['Modalidade'].unique()))
+tecnicos = filter_options(st.sidebar.selectbox("Selecione um técnico:", ['Todos'] + sorted(list(df['Técnico'].unique()))), 'Técnico')
+assentamentos = filter_options(st.sidebar.selectbox("Selecione um assentamento:", ['Todos'] + sorted(list(df['Assentamento'].unique()))), 'Assentamento')
+tipos_de_laudo = filter_options(st.sidebar.selectbox("Selecione um tipo de laudo:", ['Todos'] + sorted(list(df['Tipo de Laudo'].unique()))), 'Tipo de Laudo')
+municipios = filter_options(st.sidebar.selectbox("Selecione um município:", ['Todos'] + sorted(list(df['Município'].unique()))), 'Município')
+modalidade = filter_options(st.sidebar.selectbox("Selecione uma modalidade:", ['Todos'] + sorted(list(df['Modalidade'].unique()))), 'Modalidade')
 
 # Data inicial padrão: 01/01/2022
 start_date = datetime(2022, 1, 1).date()
@@ -27,11 +34,11 @@ start_date = datetime(2022, 1, 1).date()
 end_date = datetime.now().date()
 
 # Filtros laterais
-selected_tecnico = st.sidebar.selectbox("Selecione um técnico:", tecnicos, key="tecnico")
-selected_municipio = st.sidebar.selectbox("Selecione um município:", municipios, key="municipio")
-selected_assentamento = st.sidebar.selectbox("Selecione um assentamento:", assentamentos, key="assentamento")
-selected_tipo_laudo = st.sidebar.selectbox("Selecione um tipo de laudo:", tipos_de_laudo, key="tipo_laudo")
-selected_modalidade = st.sidebar.selectbox("Selecione uma modalidade:", modalidade, key="modalidade")
+selected_tecnico = st.sidebar.selectbox("Selecione um técnico:", tecnicos)
+selected_municipio = st.sidebar.selectbox("Selecione um município:", municipios)
+selected_assentamento = st.sidebar.selectbox("Selecione um assentamento:", assentamentos)
+selected_tipo_laudo = st.sidebar.selectbox("Selecione um tipo de laudo:", tipos_de_laudo)
+selected_modalidade = st.sidebar.selectbox("Selecione uma modalidade:", modalidade)
 
 # Filtrar por técnico
 if selected_tecnico != "Todos":
@@ -54,17 +61,17 @@ if selected_modalidade != "Todos":
     df = df[df['Modalidade'] == selected_modalidade]
 
 # Filtrar por data
-start_date = st.sidebar.date_input("Data inicial:", start_date, key="start_date")
-end_date = st.sidebar.date_input("Data final:", end_date, key="end_date")
+start_date = st.sidebar.date_input("Data inicial:", start_date)
+end_date = st.sidebar.date_input("Data final:", end_date)
 df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y').dt.date
 df = df[(df['Data'] >= start_date) & (df['Data'] <= end_date)]
 
 # Atualizar opções dos filtros com base no DataFrame filtrado
-tecnicos = ['Todos'] + sorted(list(df['Técnico'].unique()))
-municipios = ['Todos'] + sorted(list(df['Município'].unique()))
-assentamentos = ['Todos'] + sorted(list(df['Assentamento'].unique()))
-tipos_de_laudo = ['Todos'] + sorted(list(df['Tipo de Laudo'].unique()))
-modalidade = ['Todos'] + sorted(list(df['Modalidade'].unique()))
+tecnicos = filter_options(selected_tecnico, 'Técnico')
+municipios = filter_options(selected_municipio, 'Município')
+assentamentos = filter_options(selected_assentamento, 'Assentamento')
+tipos_de_laudo = filter_options(selected_tipo_laudo, 'Tipo de Laudo')
+modalidade = filter_options(selected_modalidade, 'Modalidade')
 
 # Exibir tabela interativa
 st.write(df)
